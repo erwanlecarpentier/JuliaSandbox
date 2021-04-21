a9eacce12b1cbaaf92250f0f850978b4b1d40534
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-21 15:52:31 +0200

    Runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using JuliaSandbox
+
 using Test
 
+function foo(m::Module)
+    m.foo()
+end
+
+foo(JuliaSandbox.InternalModule)
+
 @testset "JuliaSandbox.jl" begin
+    println("TODO")
     # Write your tests here.
 end
